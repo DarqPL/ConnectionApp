91054d264403d190884f9837c22ec7030f644217
WestTand <22649051.phat@student.iuh.edu.vn>
2026-02-03 01:42:08 +0700

    update config websocket

---
+++ src/main/java/iuh/fit/ConnectionAppBackend/config/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package iuh.fit.ConnectionAppBackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+    /*
+    API /topic Server → nhiều client
+    API /app Client → Server
+    API /queue Server → 1 client
+     */
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic", "/queue");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
