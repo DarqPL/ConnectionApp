c5d2f85d0df9ef37ee219b68f0645224b259bed0
WestTand <22649051.phat@student.iuh.edu.vn>
2026-03-01 22:20:47 +0700

    update register method

--- src/main/java/iuh/fit/ConnectionAppBackend/domain/dto/RegisterRequest.java
+++ src/main/java/iuh/fit/ConnectionAppBackend/domain/dto/RegisterRequest.java
@@ -8,4 +8,7 @@ import lombok.Setter;
 public class RegisterRequest {
     private String username;
     private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
 }
